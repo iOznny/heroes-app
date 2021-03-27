0f6253b68993a4758906f203e7796ed5faab1952
iOznny <fo170014@gmail.com>
2021-03-27 17:44:20 -0600

    Finday

--- src/app/heroes/pages/add/add.component.ts
+++ src/app/heroes/pages/add/add.component.ts
@@ -1,17 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+// Interfaces 
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+
+// Services
+import { HeroesService } from '../../services/heroes.service';
+
 @Component({
   selector: 'app-add',
-  templateUrl: './add.component.html',
-  styles: [
-  ]
+  templateUrl: './add.component.html'
 })
 
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  public publishers = [
+    {
+      id: 'DC',
+      description: 'DC Comics'
+    },
+    {
+      id: 'Marvel',
+      description: 'Marvel Comics'
+    }
+  ];
+
+  public heroe: Heroe = {
+    alter_ego: '',
+    superhero: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.MarvelComics,
+    alt_img: ''
+  }
+
+  constructor(private heroeService: HeroesService) { }
 
   ngOnInit(): void {
+  }
+
+  // Guardar héroe.
+  public save() {
+
+    if (this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+
+    this.heroeService.postHeroe(this.heroe).subscribe( heroe => console.log(heroe));
   }
 
 }
